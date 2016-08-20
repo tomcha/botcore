2042de6a398ec565ff8ef7096b8048c688d847e9
tomcha <tomcha@tomcha.net>
2016-08-20 17:38:52 +0900

    sakenote APIからのデータ取得テストを追加

--- spec/botcore/sakenote_api_spec.rb
+++ spec/botcore/sakenote_api_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 require 'botcore/sakenote_api'
 
 describe 'SakenoteApi' do
+  before :all do
+    @sna = Botcore::SakenoteApi.new
+  end
+
   describe '初期化もろもろ' do
     it 'apiトークンを読み込んでいる'do
-      sna = Botcore::SakenoteApi.new
-      expect(sna.loaded_apitoken?).to eq true
+      expect(@sna.loaded_apitoken?).to eq true
+    end
+  end
+
+  describe 'SakenoteApi#invest_maker' do
+    it '大阪のお酒を検索すると酒名をキーとしたハッシュが返ってくる' do
+      expect(@sna.invest_maker('大阪')).to be_an(Hash)
     end
   end
 end
