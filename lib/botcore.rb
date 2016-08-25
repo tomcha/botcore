d1708057248eec5efc4e1d8ed1eede459ff25464
tomcha <tomcha@tomcha.net>
2016-08-25 12:50:30 +0900

    設定ファイル記述修正

--- lib/botcore.rb
+++ lib/botcore.rb
@@ -1,7 +1,8 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "botcore/version"
 require "botcore/dust_calender"
 require "botcore/scrap_weather_report"
-
+require "botcore/sakenote_api"
 module Botcore
   # Your code goes here...
 end
